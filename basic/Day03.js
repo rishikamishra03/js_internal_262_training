13c87497f581a880ac6684d1683c693c3e602ad7
rishikamishra03 <mirishika7@gmail.com>
2025-12-10 05:29:54 +0000

    Day03

---
+++ basic/Day03.js
@@ -0,0 +1,28 @@
+// function Day(n){
+//     return n*2;
+// }
+// let ch=81
+// console.log(Day(ch));
+
+// let fruits=[1,2,3,4,5,"apple","mango"]
+// for(let temp of fruits){
+//     console.log(temp)
+// }
+// fruits.push("orange");
+// fruits.pop("orange")
+// fruits.unshift("mango");
+// fruits.shift()
+// fruits.indexOf("mango");
+// console.log(fruits)
+
+let nume=[10,20,30,45]
+let result=nume.filter(n=>n>20)
+console.log(result)
+
+let array1=[2,4,6,8]
+let doubled= array1.map(n=>n*2)
+console.log(doubled)
+
+let nums=[10,20,30]
+let sum=nums.reduce((acc,val)=> acc+val,0);
+console.log(sum)
